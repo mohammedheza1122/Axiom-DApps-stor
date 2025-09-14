0319676557e65c1904796f7cc23ce9a702ec60ab
Mohammad Hezam <hezammohammed416@gmail.com>
2025-09-15 02:16:37 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-// 1. استخدم الحرف الكبير في اسم الملف (Home)
-// 2. أضف امتداد الملف (.jsx)
-import Home from './pages/Home.jsx'; 
+// استورد "Home.jsx" من نفس المجلد الحالي
+import Home from './Home.jsx'; 
 
 function App() {
-  // 3. استخدم المكون بحرف كبير (PascalCase)
   return <Home />;
 }
 
 export default App;
-
